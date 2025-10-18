94211afef94fdfd808c4e573732256a6f626301e
Matthew Alexander <matthewalexander353@gmail.com>
2025-10-18 18:59:16 -0400

    Update YouTube embed link and enhance poll results section styling for improved readability and aesthetics

--- ui/static/script.js
+++ ui/static/script.js
@@ -5,7 +5,7 @@ window.onload = function () {
         const imageContainer = document.querySelector('.image-container');
         imageContainer.innerHTML = `
             <div class="youtube-embed">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/BGdsIWY9ckI?si=JKLcfd2koEmoI68-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/BGdsIWY9ckI?si=D4jrW37gCdMcO5BA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             </div>
         `;
 
